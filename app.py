046c2c129fe6f65651bbb84892cd81e973d94f0d
PaulmSchneider <pasc9915@colorado.edu>
2023-03-27 19:52:28 +0000

    Insert db

--- app.py
+++ app.py
@@ -31,3 +31,19 @@ def creating():
     conn.commit() 
     conn.close() 
     return "Basketball Table Successfully Created" 
+
+
+@app.route('/db_insert') 
+def inserting():
+    conn = psycopg2.connect("postgres://lab10_database_user:KiS4fx9OXYQtitrLjddz2sJgDUtXpFgM@dpg-cgguln0rjent5o4ovahg-a/lab10_database")
+    cur.execute('''
+        INSERT INTO Basketball (First, Last, City, Name, Number) 
+        Values 
+        ('Jayson', 'Tatum', 'Boston', 'Celtics', 0),
+        ('Stephen', 'Curry', 'San Francisco', 'Warriors', 30),
+        ('Nikola', 'Jokic', 'Denver', 'Nuggets', 15),
+        ('Kawhi', 'Leonard', 'Los Angleles', 'Clippers', 2);
+        ''')
+    conn.commit()
+    conn.close()
+    return "Basketball Table Successfully Populated" 
